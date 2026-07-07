5d2edd61dff3bc6dd38afdc1d26704b203176d08
shreyasnarlawar <narlawarshreyas@gmail.com>
2026-07-07 23:56:21 +0530

    7th commit

--- petopia_frontend/src/Component/UploadImage/uploadImage.jsx
+++ petopia_frontend/src/Component/UploadImage/uploadImage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
-import "./UploadImages.css";
+import "./UploadImage.css";
 
 const UploadImages = () => {
   const { petId } = useParams();
